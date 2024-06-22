388969dfd8a749242aeffac4e88b0007a69f91ec
Jacobo FV <priack7@hotmail.com>
2024-06-22 07:38:26 +0100

    Added loading of pre-calculated features

--- io_utils.py
+++ io_utils.py
@@ -17,3 +17,18 @@ def read_data() -> NDArray:
                            value=[0, 1, 2, 3, 4, 5], inplace=True)
     data = df.to_numpy()
     return data
+
+def read_feature_data() -> tuple[NDArray, NDArray]:
+    """
+    Reads the data from the WIDSM file, and replaces the activity categorical value ('Walking', 'Jogging', 'Upstairs',
+     'Downstairs', 'Sitting', 'Standing') for a numeric one
+    :return features: Nx42 Matrix containing N samples for each of the 42 features pre-calculated.
+    :return labels: Nx2 Matrix containing the user ID and activity for each sample.
+    """
+    df = pd.read_csv('./data/WISDM_features.txt', header=0)
+    features = df.iloc[:, 2:-2].to_numpy()
+    labels = df.iloc[:, [1, -1]]
+    labels.iloc[:, -1] = labels.iloc[:, -1].replace(
+        to_replace=['Walking', 'Jogging', 'Upstairs', 'Downstairs', 'Sitting', 'Standing'], value=[0, 1, 2, 3, 4, 5])
+    labels = labels.to_numpy()
+    return features, labels
